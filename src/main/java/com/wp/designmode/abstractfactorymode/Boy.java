04211598ffb75f6b5c0ff242a7670b31c283fe64
wangpeng116 <wangpeng116@longfor.com>
2020-01-13 19:38:48 +0800

    工厂模式

---
+++ src/main/java/com/wp/designmode/abstractfactorymode/Boy.java
@@ -0,0 +1,11 @@
+package com.wp.designmode.abstractfactorymode;
+
+/**
+ * @Classname Boy
+ * @Description 男孩
+ * @Date 2020/1/13 19:30
+ * @Created by wangpeng116
+ */
+public interface Boy {
+    void drawMan();
+}
---
+++ src/main/java/com/wp/designmode/abstractfactorymode/Girl.java
@@ -0,0 +1,11 @@
+package com.wp.designmode.abstractfactorymode;
+
+/**
+ * @Classname Girl
+ * @Description 女孩儿
+ * @Date 2020/1/13 19:31
+ * @Created by wangpeng116
+ */
+public interface Girl {
+    void drawWoman();
+}
