80070e4a10bd0a82c8bf1383c6f9834c44bc1199
wangpeng116 <wangpeng116@longfor.com>
2020-01-15 20:05:59 +0800

    策略模式

--- src/main/java/com/wp/designmode/strategymode/StrategyModeTest.java
+++ src/main/java/com/wp/designmode/strategymode/StrategyModeTest.java
@@ -4,6 +4,7 @@ import com.wp.designmode.strategymode.behaviour.impl.FlyingWithWings;
 import com.wp.designmode.strategymode.product.Duck;
 import com.wp.designmode.strategymode.product.impl.MallardDuck;
 import com.wp.designmode.strategymode.product.impl.RedheadDuck;
+import com.wp.designmode.strategymode.product.impl.RubberDuck;
 
 /**
  * @Classname StrategyModeTest
@@ -15,11 +16,13 @@ public class StrategyModeTest {
     public static void main(String[] args) {
         Duck mallardDuck = new MallardDuck();
         Duck redheadDuck = new RedheadDuck();
+        Duck rubberDuck = new RubberDuck();
         redheadDuck.display();
         redheadDuck.quack();
         //注入飞行策略
         mallardDuck.setFlyingStragety(new FlyingWithWings());
         mallardDuck.fly();
         redheadDuck.fly();
+        rubberDuck.fly();
     }
 }
---
+++ src/main/java/com/wp/designmode/strategymode/product/impl/RubberDuck.java
@@ -0,0 +1,33 @@
+package com.wp.designmode.strategymode.product.impl;
+
+import com.wp.designmode.strategymode.behaviour.FlyingStragety;
+import com.wp.designmode.strategymode.behaviour.impl.FlyingNoway;
+import com.wp.designmode.strategymode.product.Duck;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Classname RubberDuck
+ * @Description 橡胶鸭子
+ * @Date 2020/1/15 19:57
+ * @Created by wangpeng116
+ */
+@Slf4j
+public class RubberDuck extends Duck {
+
+    public RubberDuck() {
+        super();
+        super.setFlyingStragety(new FlyingNoway());
+    }
+
+    @Override
+    public void quack() {
+        log.info("嘎~嘎~嘎~");
+    }
+
+    @Override
+    public void display() {
+        log.info("我全身发黄，嘴巴很红");
+
+    }
+
+}
