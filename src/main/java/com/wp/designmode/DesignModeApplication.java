eef1e3392e54016f14464802bd664ff7a1f9533d
wangpeng116 <wangpeng116@longfor.com>
2021-01-14 15:21:45 +0800

    测试

--- src/main/java/com/wp/designmode/DesignModeApplication.java
+++ src/main/java/com/wp/designmode/DesignModeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesignModeApplication {
     public static void main(String[] args) {
+        System.out.println("我是future");
         SpringApplication.run(DesignModeApplication.class, args);
     }
 }
